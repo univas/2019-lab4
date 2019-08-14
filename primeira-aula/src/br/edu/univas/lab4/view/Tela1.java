8cd4e472fbe6e632050012ab1fe61f63fa1f2011
Rodrigo Luis de Faria <rodrigo.faria@inatel.br>
2019-08-14 20:02:48 -0300

    Add more Components to JFrame

--- primeira-aula/src/br/edu/univas/lab4/view/Tela1.java
+++ primeira-aula/src/br/edu/univas/lab4/view/Tela1.java
@@ -4,16 +4,23 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.text.NumberFormat;
 
+import javax.swing.ButtonGroup;
 import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
+import javax.swing.ScrollPaneConstants;
 
 public class Tela1 extends JFrame {
 
@@ -88,8 +95,69 @@ public class Tela1 extends JFrame {
 		courseComboBox.addItem("Matemática");
 		courseComboBox.addItem("História");
 		courseComboBox.addItem("Pedagogia");
-		courseComboBox.setPreferredSize(new Dimension(700, 40));
+		courseComboBox.setPreferredSize(new Dimension(720, 40));
 		getContentPane().add(courseComboBox);
+		
+		JCheckBox mondayCheckBox = new JCheckBox();
+		mondayCheckBox.setText("Segunda-feira");
+		mondayCheckBox.setSelected(true);
+		getContentPane().add(mondayCheckBox);
+		
+		JCheckBox tuesdayCheckBox = new JCheckBox();
+		tuesdayCheckBox.setText("Terça-feira");
+		tuesdayCheckBox.setSelected(false);
+		getContentPane().add(tuesdayCheckBox);
+		
+		JCheckBox wednesdayCheckBox = new JCheckBox();
+		wednesdayCheckBox.setText("Quarta-feira");
+		wednesdayCheckBox.setSelected(false);
+		getContentPane().add(wednesdayCheckBox);
+		
+		JLabel periodLabel = new JLabel();
+		periodLabel.setText("Período:");
+		getContentPane().add(periodLabel);
+		
+		JRadioButton morningRadioButton = new JRadioButton();
+		morningRadioButton.setText("Manhã");
+		getContentPane().add(morningRadioButton);
+		
+		JRadioButton afternoonRadioButton = new JRadioButton();
+		afternoonRadioButton.setText("Tarde");
+		getContentPane().add(afternoonRadioButton);
+		
+		JRadioButton eveningRadioButton = new JRadioButton();
+		eveningRadioButton.setText("Noite");
+		getContentPane().add(eveningRadioButton);
+		
+		ButtonGroup radioGroup = new ButtonGroup();
+		radioGroup.add(morningRadioButton);
+		radioGroup.add(afternoonRadioButton);
+		radioGroup.add(eveningRadioButton);
+		
+		JButton saveButton = new JButton();
+		saveButton.setText("Salvar");
+		getContentPane().add(saveButton);
+		
+		String[] columns = {"Nome", "E-mail", "Celular"};
+		
+		Object[][] data = {
+				{"Rodrigo", "rodrigolfsi@gmail.com", "98273-4717"},
+				{"João", "joao@gmail.com", "93123-5577"},
+				{"Maria", "maria@gmail.com", "92273-4811"}
+		};
+		
+		JTable studentTable = new JTable(data, columns);
+		
+		JScrollPane scrollStudentTable = new JScrollPane(studentTable);
+		scrollStudentTable.setPreferredSize(new Dimension(720, 150));
+		scrollStudentTable.setHorizontalScrollBarPolicy(
+				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollStudentTable.setVerticalScrollBarPolicy(
+				ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		getContentPane().add(scrollStudentTable);
+		
+		
+		
 		
 	}
 	
