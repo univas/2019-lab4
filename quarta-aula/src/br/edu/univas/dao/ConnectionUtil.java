c0e31082bcc4bd96526044d2521caee19ad45ff2
Rodrigo Luis de Faria <rodrigo.faria@inatel.br>
2019-10-26 06:31:32 -0300

    Add database connection lib

---
+++ quarta-aula/src/br/edu/univas/dao/ConnectionUtil.java
@@ -0,0 +1,24 @@
+package br.edu.univas.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+
+	private static final String USER = "postgres";
+	private static final String PASSWORD = "abc123";
+	
+	public static Connection getConnection() throws SQLException {
+		
+		String url = "jdbc:postgresql://localhost/lab4";
+		
+		Properties properties = new Properties();
+		properties.setProperty("user", USER);
+		properties.setProperty("password", PASSWORD);
+		
+		return DriverManager.getConnection(url, properties);
+	}
+	
+}
--- quarta-aula/src/br/edu/univas/main/App.java
+++ quarta-aula/src/br/edu/univas/main/App.java
@@ -1,10 +1,22 @@
 package br.edu.univas.main;
 
+import java.sql.SQLException;
+
+import br.edu.univas.dao.ConnectionUtil;
 import br.edu.univas.view.StudentFrame;
 
 public class App {
 
 	public static void main(String[] args) {
+		
+		try {
+			ConnectionUtil.getConnection();
+			System.out.println("SUCESSO!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
 		StudentFrame frame = new StudentFrame();
 		frame.setVisible(true);
 	}
